2a0ea84831093b2aa973186f3431563444b1832f
Gonçalo Amaral <goa.docs@gmail.com>
2017-03-12 17:11:47 +0000

    placed first sprite in canvas, moved game specific resources folder to game folder, centered canvas

--- flyingWig/Canvas.js
+++ flyingWig/Canvas.js
@@ -1,23 +1,25 @@
 'use strict'
 
 class Canvas {
-	constructor(root) {
+	constructor(root,width,height) {
 		//Creates canvas node
 		let _canvas = document.createElement('canvas');
+		_canvas.height = height;
+		_canvas.width = width;
 		//Mounts created canvas node in the dom and references it to this.canvas
 		this.canvas = root.appendChild(_canvas);
-		//Using flex in case we want to add out of canvas buttons
-		this.setStyle({
-			flex: '1'
-		});
+		//Define context
+		this.ctx = this.canvas.getContext('2d');
 	}
 
 	//Recieves JS plain Object and updates convas style object
 	setStyle(newStyle) {
-		let prevStyle = this.canvas.style;
-		for(let key in newStyle) {
-			prevStyle[key] = newStyle[key]
-		};
+		this.canvas.setStyle(newStyle);
+	}
+
+	drawImage(img,x,y,width,height) {
+		console.log(img);
+		this.ctx.drawImage(img,x,y,img.width,img.height);
 	}
 
 	//Get current canvas height
--- flyingWig/index.js
+++ flyingWig/index.js
@@ -1,15 +1,34 @@
 'use strict'
+//NOTE: When arrow functions are used in global scope, they always refer to the global this, window. For safety always use ES5 anonymous function calls in global scope.
+//NOTE: Auxiliar variables should hava a _ before their proper name, for example, _auxVar instead of auxVar
+//TODO: Make game responsive
 
-window.onload = function() {
+
+//Update style prop object from given object
+Object.prototype.setStyle = function(newStyle) {
+	let prevStyle = this.style;
+	if(this.style!=undefined) {
+		for(let key in newStyle) {
+			prevStyle[key] = newStyle[key];
+		}
+	}
+}
+
+//On window full load
+this.onload = function() {
 	let main = document.getElementsByTagName("main")[0];
-	//Sets main to fill entire screen and take display as flex
-	main.style='display: flex; width: 100%; height: 100%;';
 
 	//create canvas and set canvas style
-	let canvas = new Canvas(main);
+	let canvas = new Canvas(main,640,360);
 	canvas.setStyle({
 		border: '1px solid #000000',
-		backgroundColor: 'yellow'
+		margin: 'auto'
 	});
 
+	//Load stand.png and draw it in the canvas left down corner
+	let _img = new Image(481/7,545/7);
+	_img.onload = () => {
+		canvas.drawImage(_img,0,canvas.getHeight()-_img.height,_img.width,_img.height);
+	}
+	_img.src = './resources/images/sprites/stand.png';
 };
