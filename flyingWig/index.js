e99897020911f867132f79c050dd8557057fdc50
Goncalo Amaral <goa.docs@gmail.com>
2017-03-23 15:20:22 +0000

    Sprites laoding fully working and desert background image

--- flyingWig/index.js
+++ flyingWig/index.js
@@ -17,61 +17,132 @@ this.onload = () => {
 	// create canvas and set canvas style
 	let canvas = new Canvas(main, 640, 360);
 
-	//Load sprites
-	let spritesPath = './resources/images/sprites/';
+	// Palin JS object of fully loaded resources
+	let resources = {};
+	//Number of tasks to complete
+	let totalTasksToComplete = 2;
+	// Create event to signal end of images loading
+	window.addEventListener('imageLoadingComplete', imageLoadingCompleteHandler);
 
+	//Creates event to signal end of sprite images loading
+	window.addEventListener('spritesLoaded', spritesLoadedHandler);
+	//Load sprite images
+	let spriteNodesObject = loadSpriteImages();
+
+	//Create event to signal end of background images loading
+	window.addEventListener('backgroundsLoaded', backgroundsLoadedHandler);
+	//Load background images
+	let backgroundNodesObject = loadBackgroundImages();
+
+	// Waits for event spritesLoaded event to be triggered
+	function spritesLoadedHandler() {
+		window.removeEventListener('spritesLoaded', spritesLoadedHandler);
+	 	resources['sprites'] = spriteNodesToSpriteObjects(spriteNodesObject, main);
+		if(Object.keys(resources).length == totalTasksToComplete) {
+			let imageLoadingCompleteEvent = new Event('imageLoadingComplete');
+			window.dispatchEvent(imageLoadingCompleteEvent);
+		}
+	}
+
+	// Waits for event backgroundsLoaded event to be triggered
+	function backgroundsLoadedHandler() {
+		window.removeEventListener('backgroundsLoaded', backgroundsLoadedHandler);
+		resources['backgrounds'] = backgroundNodesObject;
+		if(Object.keys(resources).length == totalTasksToComplete) {
+			let imageLoadingCompleteEvent = new Event('imageLoadingComplete');
+			window.dispatchEvent(imageLoadingCompleteEvent);
+		}
+	}
+
+	// Waits for event imageLoadingComplete event to be triggered
+	function imageLoadingCompleteHandler() {
+		window.removeEventListener('imageLoadingComplete', imageLoadingCompleteHandler);
+		imageLoadingComplete(canvas, resources);
+	}
+}
+
+//Load sprite images
+function loadSpriteImages() {
+	// Sprites path
+	let spritesPath = './resources/images/sprites/';
 	// Sprite names
 	let spriteNames = [ 'stand','jump','slide','anim1','anim2','anim3' ];
-
-	// Create auxiliary tag to listen to events
-	let listener = document.createElement('listener');
-
 	// Load every sprite and create a image node for each one
-	let spriteNodesObject = createSpriteNodes(spriteNames, spritesPath, listener);
+	let spriteNodesObject = createImageNodes(spriteNames, spritesPath, 'sprites');
 
-	// Waits for even spritesLoaded to be triggered
-	listener.addEventListener('spritesLoaded', spritesLoadedHandler);
-	function spritesLoadedHandler() {
-		console.log('spritesLoadedHandler');
-		listener.removeEventListener('spritesLoaded', spritesLoadedHandler);
-		spriteNodesToSpriteObjects(main, canvas, spriteNodesObject, listener);
-	}
+	return spriteNodesObject;
+}
+
+//Load background images
+function loadBackgroundImages() {
+	// Backgrounds path
+	let backgroundsPath = './resources/images/backgrounds/';
+	// Background names
+	let backgroundNames = [ 'desert' ];
+	// Load every background and create a image node for each one
+	let backgroundNodesObject = createImageNodes(backgroundNames, backgroundsPath, 'backgrounds');
+
+	return backgroundNodesObject;
 }
 
 // Load every sprite and create a image node for each one
 // NOT RESPONSIVE
-function createSpriteNodes(spriteNames, spritesPath, listener) {
+function createImageNodes(names, path, mode) {
 	let out = {};
 	let count = 0;
 
 	// Create nodes
-	for (let ind in spriteNames) {
-		let spriteName = spriteNames[ind];
+	for (let ind in names) {
+		let name = names[ind];
 		let node = new Image();
 		node.onload = onloadHandler;
-		node.src = spritesPath + spriteName + '.png';
-		out[spriteName] = node;
+		node.src = path + name + '.png';
+		out[name] = node;
 	}
 
 	return out;
 
 	function onloadHandler(ev) {
 		let targ = ev.target;
-		//NOT RESPONSIVE
-		targ.height = targ.naturalHeight/7;
-		//NOT RESPONSIVE
-		targ.width = targ.naturalWidth/7;
+		if(mode == 'sprites') {
+			//NOT RESPONSIVE
+			targ.height = targ.naturalHeight/7;
+			//NOT RESPONSIVE
+			targ.width = targ.naturalWidth/7;
+		} else if(mode == 'backgrounds') {
+			//NOT RESPONSIVE
+			targ.height = targ.naturalHeight;
+			//NOT RESPONSIVE
+			targ.width = targ.naturalWidth;
+		}
 		++count;
-		if(count == spriteNames.length) {
-			let spritesLoadedEvent = new Event('spritesLoaded');
-			listener.dispatchEvent(spritesLoadedEvent);
+		if(count == names.length) {
+			if(mode == 'sprites') {
+				let spritesLoadedEvent = new Event('spritesLoaded');
+				window.dispatchEvent(spritesLoadedEvent);
+			} else if(mode == 'backgrounds') {
+				let backgroundsLoadedEvent = new Event('backgroundsLoaded');
+				window.dispatchEvent(backgroundsLoadedEvent);
+			}
 		}
 	}
 }
 
-function spriteNodesToSpriteObjects(main, canvas, spriteNodesObject, listener) {
-	// Create sprite objects
-	let sprites = createSpriteObjects(spriteNodesObject, main);
+// Create Sprite objects for each sprite ndoe
+function spriteNodesToSpriteObjects(spriteNodesObject, root) {
+	let out = {};
+	// Create Sprite objects
+	for (let spriteName in spriteNodesObject) {
+		let img = spriteNodesObject[spriteName];
+		out[spriteName] = new Sprite(root, img);
+	}
+	return out;
+}
+
+function imageLoadingComplete(canvas, resources) {
+	let { sprites, backgrounds } = resources;
+	// Set canvas background
+	//canvas.setBackground(backgrounds.desert);
 
 	// Draw sprite on canvas
 	canvas.drawSprite(sprites.stand, 0, 0);
@@ -84,15 +155,4 @@ function spriteNodesToSpriteObjects(main, canvas, spriteNodesObject, listener) {
 	canvas.drawSprite(sprites.slide, 400, 100);
 
 	//NOTE: CONTINUE CODE HERE
-};
-
-// Create Sprite objects for each sprite
-function createSpriteObjects(spriteNodesObject, main) {
-	let out = {};
-	// Create Sprite objects
-	for (let spriteName in spriteNodesObject) {
-		let img = spriteNodesObject[spriteName];
-		out[spriteName] = new Sprite(main, img);
-	}
-	return out;
 }
