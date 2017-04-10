f3a066122e89f1edf2c75ce33c8d613eec0a16e3
Gonçalo Amaral <goa.docs@gmail.com>
2017-04-10 14:22:05 +0100

    removed canvas onload handlerr and cross origin property from canvas...

--- scripts/Sprite.js
+++ scripts/Sprite.js
@@ -2,7 +2,7 @@
 
 class Sprite {
   constructor(root, img) {
-    // Convert constructor args as class properties
+    //Convert constructor args as class properties
     this.root = root;
     this.img = img;
     this.width = img.width;
@@ -11,17 +11,18 @@ class Sprite {
   }
 
   getImageData() {
+    // If is running for the first time (by the constructor)
     if(this.imgData === undefined) {
+      //Creates aux canvas and fetchs canvas data
       let _canvas = new Canvas(this.root, this.width, this.height);
-      _canvas.onload = function() {
-        _canvas.unmountCanvas();
-        _canvas.drawImage(this.img, 0, 0, this.width, this.height);
-      };
-      _canvas.crossOrigin = "anonymous";
+      _canvas.unmountCanvas();
+      _canvas.drawImage(this.img, 0, 0, this.width, this.height);
       return _canvas.getFrameData();
-    }
-    else {
+    } else {
+      // imgData already calculated, return imgData, this avoids unnecessary computing
       return this.imgData;
     }
   }
 }
+
+//export default Sprite;
