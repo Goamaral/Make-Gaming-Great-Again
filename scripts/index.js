a4046891b88ecbaad206e0b97a3904c5ede19a16
Goncalo Amaral <goa.docs@gmail.com>
2017-04-10 23:11:26 +0100

    Added audio mute and unmute control in settings and created cross iframes button handler

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,165 @@
 "use strict";
 
-(function() {
-	window.addEventListener("load", main);
-}());
+window.onload = function() {
+  //Get Audio node
+  let audio = document.getElementsByTagName('audio')[0];
 
-function main() {
-	showPage("menu.html");
+  let main = document.getElementsByTagName('main')[0];
+
+  // Create all screens
+  let iframeMenu = document.createElement("iframe");
+  iframeMenu.src = 'menu.html';
+
+  let iframeStoryGame = document.createElement("iframe");
+  iframeStoryGame.src = 'storyGame.html';
+
+  let iframeInfiniteGame = document.createElement("iframe");
+  //iframeInfiniteGame.src = 'infiniteGame.html';
+
+  let iframeHowToPlay = document.createElement("iframe");
+  //iframeHowToPlay.src = 'howToPlay.html';
+
+  let iframeHighscores = document.createElement("iframe");
+  //iframeHighscores.src = 'highscores.html';
+
+  let iframeSettings = document.createElement("iframe");
+  iframeSettings.src = 'settings.html';
+
+  let nodes = {
+    iframeMenu: iframeMenu,
+    iframeStoryGame: iframeStoryGame,
+    iframeInfiniteGame: iframeInfiniteGame,
+    iframeHowToPlay: iframeHowToPlay,
+    iframeHighscores: iframeHighscores,
+    iframeSettings: iframeSettings,
+    audio: audio,
+    main: main
+  };
+
+  main.appendChild(iframeMenu);
+  iframeMenu.onload = function() {
+    menuHandler(nodes);
+  }
 }
 
-// Navegação e gestão de janelas
-function showPage(page) {
-	// carregar página na frame e enviar mensagem para a página logo que esteja carregada (frameLoadHandler)
-	var frm = document.getElementsByTagName("iframe")[0];
-	frm.src = page;
+function menuHandler(nodes) {
+  let {
+    iframeMenu,
+    iframeStoryGame,
+    iframeInfiniteGame,
+    iframeHowToPlay,
+    iframeHighscores,
+    iframeSettings,
+    main
+  } = nodes;
+
+  let iframeMenuDocument = getDocument(iframeMenu);
+
+  //Buttons
+  let storyGameButton = iframeMenuDocument.getElementById('storyGameButton');
+  storyGameButton.onclick = buttonHandler;
+
+  let infiniteGameButton = iframeMenuDocument.getElementById('infiniteGameButton');
+  infiniteGameButton.onclick = buttonHandler;
+
+  let howToPlayButton = iframeMenuDocument.getElementById('howToPlayButton');
+  howToPlayButton.onclick = buttonHandler;
+
+  let highscoresButton = iframeMenuDocument.getElementById('highscoresButton');
+  highscoresButton.onclick = buttonHandler;
+
+  let settingsButton = iframeMenuDocument.getElementById('settingsButton');
+  settingsButton.onclick = buttonHandler;
+
+  function buttonHandler(ev){
+    switch (ev.target.id) {
+      case 'storyGameButton':
+        replaceChildren(main, iframeMenu, iframeStoryGame);
+        iframeStoryGame.onload = function() {
+          gameHandler(nodes);
+        }
+        break;
+      case 'infiniteGameButton':
+        replaceChildren(main, iframeMenu, iframeInfiniteGame);
+        iframeInfiniteGame.onload = function() {
+          gameHandler(nodes);
+        }
+        break;
+      case 'howToPlayButton':
+        replaceChildren(main, iframeMenu, iframeHowToPlay);
+        iframeHowToPlay.onload = function() {
+          howToPlayHandler(nodes);
+        }
+        break;
+      case 'highscoresButton':
+        replaceChildren(main, iframeMenu, iframeHighscores);
+        iframeHighscores.onload = function() {
+          highscoresHandler(nodes);
+        }
+        break;
+      case 'settingsButton':
+        replaceChildren(main, iframeMenu, iframeSettings);
+        iframeSettings.onload = function() {
+          settingsHandler(nodes);
+        }
+        break;
+      default:
+        console.log('On click event poorly configured -> buttonHandler');
+        return;
+    }
+  }
+}
+
+function gameHandler(nodes) {
+
+}
+
+function howToPlayHandler(nodes) {
+
+}
+
+function highscoresHandler(nodes) {
+
+}
+
+function settingsHandler(nodes) {
+  let { iframeSettings, audio, iframeMenu, main } = nodes;
+  let iframeSettingsDocument = getDocument(iframeSettings);
+
+  //Buttons
+  let toggleAudioButton = iframeSettingsDocument.getElementById('muteButton');
+  toggleAudioButton.onclick = buttonHandler;
+
+  let backButton = iframeSettingsDocument.getElementById('backButton');
+  backButton.onclick = buttonHandler;
+
+  function buttonHandler(ev) {
+    switch (ev.target.id) {
+      case 'muteButton':
+        toggleAudio(audio)
+        break;
+      case 'backButton':
+        replaceChildren(main, iframeSettings, iframeMenu);
+        iframeMenu.onload = function() {
+          menuHandler(nodes);
+        }
+        break;
+      default:
+        console.log('On click event poorly configured -> buttonHandler');
+        return;
+    }
+  }
+}
+
+function replaceChildren(root, from, to) {
+  root.removeChild(from);
+  root.appendChild(to);
+}
+
+function getDocument(iframe) {
+  return iframe.contentDocument || iframe.contentWindow.document;
+}
+
+function toggleAudio(audio) {
+  audio.muted = !audio.muted;
 }
