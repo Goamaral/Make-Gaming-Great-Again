e2e782a33e442ceeec89b31b970e61cf5c6036be
Gonçalo Amaral <goa.docs@gmail.com>
2017-04-16 23:29:09 +0100

    improves onclick button feedback

--- scripts/highscores.js
+++ scripts/highscores.js
@@ -5,6 +5,13 @@ window.onload = function() {
     buttons.push(document.getElementById('backButton'));
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].onclick = buttonClickHandler;
+        for (let j = 0; j < buttons[i].children.length; ++j ) {
+          buttons[i].children[j].onclick = buttonClickChildHandler;
+        }
+    }
+
+    function buttonClickChildHandler(ev) {
+      talkWithParent(ev.target.parentNode.id);
     }
 
     function buttonClickHandler(ev) {
