ed04cd39480a422b00025ae1f8f743e47f3a7364
teresalazar13 <teresa.sal13@gmail.com>
2017-05-06 17:08:32 +0100

    Adds image and backButton to EndOfGame.

---
+++ scripts/endOfGame.js
@@ -0,0 +1,19 @@
+"use strict";
+
+window.onload = function() {
+    let buttons = [];
+    buttons.push(document.getElementById('backButton'));
+    buttons[0].onclick = buttonClickHandler;
+
+    function buttonClickChildHandler(ev) {
+      talkWithParent(ev.target.parentNode.id);
+    }
+
+    function buttonClickHandler(ev) {
+        talkWithParent(ev.target.id)
+    }
+};
+
+function talkWithParent(msg) {
+    parent.postMessage(msg, '*');
+}
