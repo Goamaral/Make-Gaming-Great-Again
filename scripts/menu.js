3ff0e5792b7ccf16258cd2193149a22164e27a68
Miguel Machado <mantonio@student.dei.uc.pt>
2017-05-08 17:31:15 +0100

    Comments

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,29 +3,37 @@
 window.onload = function() {
   let buttons = document.getElementsByTagName('button');
   let audio = {};
+  
   for ( let i = 0; i < buttons.length; i++ ) {
     buttons[i].onclick = buttonClickHandler;
+    
     for (let j = 0; j < buttons[i].children.length; ++j ) {
       buttons[i].children[j].onclick = buttonClickChildHandler;
     }
+    
     buttons[i].onmouseover = playSoundHandler;
     audio[buttons[i].id] = document.createElement('audio');
     audio[buttons[i].id].src = 'resources/sounds/hover-sound.mp3';
   }
 
+  // Play sound on button hover
   function playSoundHandler(ev) {
     playSound(audio[ev.target.id]);
   }
 
+  // Handle click inside paragraph of button
   function buttonClickChildHandler(ev) {
     talkWithParent(ev.target.parentNode.id);
   }
 
+  // Handle click in button
   function buttonClickHandler(ev) {
     talkWithParent(ev.target.id)
   }
 };
 
+// Send message to parent index,js.
+// Index.js is the parent because menu.html is being loaded inside of index.html
 function talkWithParent(msg) {
   parent.postMessage(msg, '*');
 }
